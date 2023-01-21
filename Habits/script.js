abeeb0478075463c741e96464377d3f65bfa576b
BrendaGuedes <guedesbrenda23@gmail.com>
2023-01-21 12:00:42 -0300

    NLW4

--- Habits/script.js
+++ Habits/script.js
@@ -1,11 +1,31 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  bath: ["01-01", "01-02", "01-04", "01-05", "01-06", "01-07", "01-08", '01-09'],
-  food: ["01-01", "01-02", "01-03"],
-  study: ["01-04", "01-01"],
-  music: ["01-05"],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-BR').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('Dia já incluso')
+    return
+  }
+
+  alert ('☑')
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  // console.log(nlwSetup.data)
+
+  localStorage.setItem("NLWSetup@habits", JSON.stringify 
+  (nlwSetup.data))
+  
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
